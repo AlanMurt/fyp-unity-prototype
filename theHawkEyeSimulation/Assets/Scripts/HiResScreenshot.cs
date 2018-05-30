64ad75e5ee275c7e66d94c0af430925a7f36aeb2
Alan Moriarty <t00175140@365s.ittralee.ie>
2018-05-30 16:37:53 +0100

    Contains my Unity Prototype for my FYP.

---
+++ theHawkEyeSimulation/Assets/Scripts/HiResScreenshot.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiResScreenshot : MonoBehaviour {
+
+
+    // public Transform Camera1, Camera2, Ball;
+    Camera cam1; //, cam2;
+    // Use this for initialization
+
+    public int resWidth = 2550;
+    public int resHeight = 3300;
+
+    private bool takeHiResShot = false;
+    public void Start()
+    {
+
+        cam1 = GetComponent<Camera>();
+    }
+    public static string ScreenShotName(int width, int height)
+    {
+        return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
+                             Application.dataPath,
+                             width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void TakeHiResShot()
+    {
+        takeHiResShot = true;
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C)) TakeHiResShot();
+
+
+    }
+    void LateUpdate()
+    {
+        takeHiResShot |= Input.GetKeyDown("k");
+        if (takeHiResShot)
+        {
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            cam1.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            cam1.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            cam1.targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = ScreenShotName(resWidth, resHeight);
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log(string.Format("Took screenshot to: {0}", filename));
+            takeHiResShot = false;
+        }
+    }
+}
---
+++ theHawkEyeSimulation/Assets/Scripts/cameraControl.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour {
+
+    public Transform Camera1, Camera2, Ball;
+    Camera cam1, cam2;
+    // Use this for initialization
+
+    GameObject ourGuess;
+    void Start() {
+        //ourGuess = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+
+        cam1 = Camera1.gameObject.GetComponent<Camera>();
+        cam2 = Camera2.gameObject.GetComponent<Camera>();
+    } 
+
+    // Update is called once per frame
+    void Update() {
+
+
+
+        Vector2 simulatedPositionOnScreenFromCam1 = cam1.WorldToScreenPoint(Ball.position);
+
+        simulatedPositionOnScreenFromCam1 = new Vector2(Mathf.RoundToInt(simulatedPositionOnScreenFromCam1.x) + 5.0f, Mathf.RoundToInt(simulatedPositionOnScreenFromCam1.y) - 4.0f);
+
+
+        Vector2 simulatedPositionOnScreenFromCam2 = cam2.WorldToScreenPoint(Ball.position);
+
+        simulatedPositionOnScreenFromCam2 = new Vector2(Mathf.RoundToInt(simulatedPositionOnScreenFromCam2.x), Mathf.RoundToInt(simulatedPositionOnScreenFromCam2.y));
+
+
+        Ray Camera1Ray = cam1.ScreenPointToRay(simulatedPositionOnScreenFromCam1);
+
+        Ray Camera2Ray = cam2.ScreenPointToRay(simulatedPositionOnScreenFromCam2);
+
+        Debug.DrawRay(Camera1Ray.origin, 100 * Camera1Ray.direction, Color.blue);
+
+
+        Debug.DrawRay(Camera2Ray.origin, 100 * Camera2Ray.direction, Color.red);
+
+        //ourGuess.transform.position = nearestPoint(Camera1Ray, Camera2Ray);
+
+        //print(cam2.WorldToScreenPoint(Ball.position));
+        print(nearestPoint(Camera1Ray, Camera2Ray));
+    }
+
+    Vector3 nearestPoint(Ray cam1, Ray cam2)
+    {
+        Vector3 D1, D2, D3, P1, P2;
+
+        P1 = cam1.origin;
+        P2 = cam2.origin;
+        D1 = cam1.direction;
+        D2 = cam2.direction;
+
+        D3 = Vector3.Cross(D1, D2);
+
+        Vector4 col1 = new Vector4(D1.x, -D2.x, D3.x, 0);
+        Vector4 col2 = new Vector4(D1.y, -D2.y, D3.y, 0);
+        Vector4 col3 = new Vector4(D1.z, -D2.z, D3.z, 0);
+        Vector4 col4 = new Vector4(0, 0, 0, 1);
+
+
+
+        Matrix4x4 m = new Matrix4x4(col1, col2, col3, col4);
+
+        m = m.transpose;
+
+        Matrix4x4 inv = m.inverse;
+
+        Vector4 constants = new Vector4((P2 - P1).x, (P2 - P1).y, (P2 - P1).z, 0);
+        Vector4 soln = inv * constants;
+
+        return ((P1 + soln.x * D1) + (P2 + soln.y * D2)) / 2;
+
+
+
+
+    }
+
+
+
+}
